f0ec3130d46eedc793122d652904e75f09648022
Ayşe BAYRAK <202803003@ogr.cbu.edu.tr>
2021-06-10 00:29:31 +0300

    .

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,12 +1,12 @@
 import React, { useState,useEffect } from "react";
+import { Link } from "react-router-dom";
 import { Icon, Label, Menu, Table } from "semantic-ui-react";
 import ProductService from "../services/productService";
 
 export default function ProductList() {
   const [products, setProducts] = useState([]);
 
-  useEffect(()=>{   //component yüklendiğinde yapılmasını istediğin kodu buraya yazacaksın
-
+  useEffect(()=>{
     let productService = new ProductService()
     productService.getProducts().then(result=>setProducts(result.data.data))
   },[])
@@ -23,11 +23,10 @@ export default function ProductList() {
             <Table.HeaderCell>Kategori</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
-
         <Table.Body>
           {products.map((product) => (
             <Table.Row key={product.id}>
-              <Table.Cell>{product.productName}</Table.Cell>
+              <Table.Cell><Link to={`/products/${product.productName}`}>{product.productName}</Link></Table.Cell>
               <Table.Cell>{product.unitPrice}</Table.Cell>
               <Table.Cell>{product.unitsInStock}</Table.Cell>
               <Table.Cell>{product.quantityPerUnit}</Table.Cell>
