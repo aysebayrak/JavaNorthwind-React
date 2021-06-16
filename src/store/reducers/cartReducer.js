c719835324acb80c4c8ea1cc9f76e02ab3bc955b
Ayşe BAYRAK <202803003@ogr.cbu.edu.tr>
2021-06-16 22:42:29 +0300

    .

---
+++ src/store/reducers/cartReducer.js
@@ -0,0 +1,23 @@
+import { ADD_TO_CART } from "../actions/cartActions";
+import { cartItems } from "../initialValues/cartItem";
+
+const initialState={
+    cartItems:cartItems
+}
+
+
+export default function cartReducer(state=initialState,{type,payload}){
+    switch (key) {
+        case ADD_TO_CART:
+            let product = state.cartItems.find(c=>c.product.id==payload.id)
+            
+            break;
+    
+        default:
+            break;
+    }
+
+
+    
+
+}
