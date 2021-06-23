385b147e5aab85b15a7bcd6d77e63aec1907ce5a
Ayşe BAYRAK <202803003@ogr.cbu.edu.tr>
2021-06-23 21:41:26 +0300

    .

--- src/layouts/CartSummary.js
+++ src/layouts/CartSummary.js
@@ -1,32 +1,27 @@
-  
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom';
-import { Dropdown } from "semantic-ui-react";
+import {  NavLink } from 'react-router-dom';
+import { Dropdown,Label } from "semantic-ui-react";
 import { useSelector } from 'react-redux';
+
+
 export default function CartSummary() {
 
- 
-  
-   const  {cartItems} = useSelector(state => state.cart)
+   const {cartItems} = useSelector(state => state.cart)
+
     return (
         <div>
             <Dropdown item text="Sepetiniz">
               <Dropdown.Menu>
                 {
-                  cartItems.map((cartItem) =>(
-                    <Dropdown.Item  key={cartItem.product.id}>
+                  cartItems.map((cartItem)=>(
+                    <Dropdown.Item>
                       {cartItem.product.productName}
                       <Label>
                         {cartItem.quantity}
-
                       </Label>
-
                     </Dropdown.Item>
                   ))
                 }
-                <Dropdown.Item>Acer Laptop</Dropdown.Item>
-                <Dropdown.Item>Asus Laptop</Dropdown.Item>
-                <Dropdown.Item>Dell Laptop</Dropdown.Item>
                 <Dropdown.Divider/>
                 <Dropdown.Item as={NavLink} to="/cart">Sepete git</Dropdown.Item>
               </Dropdown.Menu>
